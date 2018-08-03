4120aa81c25f1505336a847e0e778aa750dc9607
jung.sangeun <jung.sangeun@nbt.com>
2018-08-03 16:05:03 +0900

    first commit

--- hotnono/ViewController.swift
+++ hotnono/ViewController.swift
@@ -10,9 +10,19 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var containerView: UIView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+    }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        let webView = UIWebView(frame: CGRect(x: 0, y: 0, width: self.view.frame.size.width, height: self.view.frame.size.height - 50))
+        webView.backgroundColor = UIColor.clear
+        containerView.addSubview(webView)
+        
+        webView.loadRequest(URLRequest(url: URL(string: "https://maps.darksky.net/@temperature,40.219,-231.200,4")!))
     }
 
     override func didReceiveMemoryWarning() {
