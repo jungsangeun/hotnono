535419e2eff393fb6e228ca7fb01b845d3ea7c02
jung.sangeun <jung.sangeun@nbt.com>
2018-08-03 19:39:44 +0900

    추가 : Firebase 테스트 클래스를 추가한다.

---
+++ hotnono/Controller/Main/DatabaseViewController.swift
@@ -0,0 +1,13 @@
+//
+//  AdMobViewController.swift
+//  hotnono
+//
+//  Created by jungsangeun on 2018. 8. 3..
+//  Copyright © 2018년 jungsangeun. All rights reserved.
+//
+
+import UIKit
+
+class DatabaseViewController: BaseViewController {
+    
+}
