f11379b2f04ca36a44d9169785b6c8eb957cb65e
jung.sangeun <jung.sangeun@nbt.com>
2018-08-08 21:52:14 +0900

    수정 : 회원가입 로직을 수정한다.

--- hotnono/Controller/BaseViewController.swift
+++ hotnono/Controller/BaseViewController.swift
@@ -13,4 +13,21 @@ class BaseViewController: UIViewController {
     override func viewDidLoad() {
         
     }
+    
+    func showAlertPopup(message: String) {
+        let alertController = UIAlertController(title: "알림", message: message, preferredStyle: .alert)
+        
+        // Create the actions
+        let okAction = UIAlertAction(title: "확인", style: UIAlertActionStyle.default) {
+            UIAlertAction in
+            self.dismiss(animated: true, completion: nil)
+        }
+        
+        // Add the actions
+        alertController.addAction(okAction)
+        
+        // Present the controller
+        self.present(alertController, animated: true, completion: nil)
+
+    }
 }
--- hotnono/Controller/Main/MainViewController.swift
+++ hotnono/Controller/Main/MainViewController.swift
@@ -16,9 +16,14 @@ class MainViewController: BaseViewController {
     @IBOutlet weak var passwordTextField: UITextField!
     
     @IBAction func clickEmailJoin(_ sender: Any) {
+        let email = emailTextField.text ?? ""
+        let password = passwordTextField.text ?? ""
+        
+        signUp(email, password)
     }
     
     @IBAction func clickGoogleJoin(_ sender: Any) {
+        
     }
     
     
@@ -26,26 +31,28 @@ class MainViewController: BaseViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
     }
     
     override func viewDidAppear(_ animated: Bool) {
-        stateLabel.text = "로그인\n확인 중.."
+        
     }
 
     override func viewWillAppear(_ animated: Bool) {
+        stateLabel.text = "로그인\n확인 중."
+        
         handleAuth = Auth.auth().addStateDidChangeListener{
             (auth, user) in
-                dump(user)
+                print("addStateDidChangeListener ")
+                print(user)
         }
         
         if let user = Auth.auth().currentUser {
             // User is signed in.
-            stateLabel.text = "확인 완료.\n잠시만\n기다려주세요."
-            
+            stateLabel.text = "잠시만\n기다려\n주세요."
+            moveNext(user: user)
         } else {
             // No user is signed in.
-            stateLabel.text = "환영합니다."
+            stateLabel.text = "환영\n합니다."
         }
     }
     
@@ -56,6 +63,48 @@ class MainViewController: BaseViewController {
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
+    }
+    
+    private func signUp(_ email: String, _ password: String) {
+        Auth.auth().createUser(withEmail: email, password: password) { (user, error) in
+            if let error = error {
+                if let errCode = AuthErrorCode(rawValue: error._code) {
+                    switch errCode {
+                    case .emailAlreadyInUse :
+                        self.signIn(email, password)
+                    default:
+                        self.stateLabel.text = "오류:("
+                        self.showAlertPopup(message: error.localizedDescription)
+                    }
+                }
+            } else {
+                if let user = user?.user {
+                    self.showAlertPopup(message: "회원 가입 완료!")
+                    self.moveNext(user: user)
+                } else {
+                    self.showAlertPopup(message: "다시 시도해 주세요 :(")
+                }
+            }
+        }
+    }
+    
+    private func signIn(_ email: String, _ password: String) {
+        Auth.auth().signIn(withEmail: email, password: password) { (user, error) in
+            if let error = error {
+                self.stateLabel.text = "오류:("
+                self.showAlertPopup(message: error.localizedDescription)
+            } else {
+                if let user = user?.user {
+                    self.showAlertPopup(message: "로그인 완료!")
+                    self.moveNext(user: user)
+                } else {
+                    self.showAlertPopup(message: "다시 시도해 주세요 :(")
+                }
+            }
+        }
+    }
+    
+    private func moveNext(user: User) {
     }
 }
 
